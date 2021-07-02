87445fdc641b8ab7bf2121873443e54619e9846f
박성재 <@210.178.237.79>
2021-07-02 09:10:10 +0900

    1

---
+++ 20210702-클래스의종류(추상클래스,인터페이스,내부클래스)/src/com/sist/main/클래스의종류.java
@@ -0,0 +1,16 @@
+package com.sist.main;
+/*
+ *	1. 변수
+ *	2. 연산자, 제어문
+ *	==============
+ *	3. 메소드
+ *	4. 
+ */
+public class 클래스의종류 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
